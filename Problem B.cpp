607c2d439f7569742a5030e4a2a32e85e22c79b0
Nivitha <nivivijay2901@gmail.com>
2026-06-27 16:11:36 +0530

    Create Problem B.cpp

---
+++ Problem B.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> freq;
+        int n = nums.size();
+        for(int x:nums){
+            freq[x]++;
+        }
+        vector<vector<int>> bucket(n+1);
+        for(auto it:freq){
+            bucket[it.second].push_back(it.first);
+        }
+        vector<int> ans;
+        for(int i = n;i>=0 && ans.size()<k;i--){
+            for(int x:bucket[i]){
+                ans.push_back(x);
+                if(ans.size()==k){
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
